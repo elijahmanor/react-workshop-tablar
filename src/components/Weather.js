90968e284ec31f8185bc98f4a0483716d91f95be
Elijah Manor <elijah.manor@gmail.com>
2019-04-25 21:49:37 -0500

    Fixed date format

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -77,8 +77,8 @@ function DetailedForecast({ weather }) {
             <path d="M22.088 13.126l1.912-1.126-1.912-1.126c-1.021-.602-1.372-1.91-.788-2.942l1.093-1.932-2.22-.02c-1.185-.01-2.143-.968-2.153-2.153l-.02-2.219-1.932 1.093c-1.031.583-2.34.233-2.941-.788l-1.127-1.913-1.127 1.913c-.602 1.021-1.91 1.372-2.941.788l-1.932-1.093-.02 2.219c-.01 1.185-.968 2.143-2.153 2.153l-2.22.02 1.093 1.932c.584 1.032.233 2.34-.788 2.942l-1.912 1.126 1.912 1.126c1.021.602 1.372 1.91.788 2.942l-1.093 1.932 2.22.02c1.185.01 2.143.968 2.153 2.153l.02 2.219 1.932-1.093c1.031-.583 2.34-.233 2.941.788l1.127 1.913 1.127-1.913c.602-1.021 1.91-1.372 2.941-.788l1.932 1.093.02-2.219c.011-1.185.969-2.143 2.153-2.153l2.22-.02-1.093-1.932c-.584-1.031-.234-2.34.788-2.942zm-10.117 6.874c-4.411 0-8-3.589-8-8s3.588-8 8-8 8 3.589 8 8-3.589 8-8 8zm.029-2c-3.313 0-6-2.687-6-6s2.687-6 6-6v12z" />
           </svg>
           <span>
-            {new Date(weather.sys.sunrise).toLocaleTimeString()} -{" "}
-            {new Date(weather.sys.sunset).toLocaleTimeString()}
+            {new Date(weather.sys.sunrise * 1000).toLocaleTimeString()} -{" "}
+            {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}
           </span>
         </div>
       </div>
@@ -90,37 +90,33 @@ function DetailedForecast({ weather }) {
 const getWeather = throttle(url => {
   console.log("actually doing the ajax call...");
   const data = {
-    coord: { lon: -0.13, lat: 51.51 },
+    coord: { lon: -86.78, lat: 36.16 },
     weather: [
-      {
-        id: 300,
-        main: "Drizzle",
-        description: "light intensity drizzle",
-        icon: "09d"
-      }
+      { id: 501, main: "Rain", description: "moderate rain", icon: "10n" }
     ],
     base: "stations",
     main: {
-      temp: Math.random() * 100,
-      pressure: 1012,
-      humidity: 81,
-      temp_min: 279.15,
-      temp_max: 281.15
+      temp: 60.69,
+      pressure: 1008,
+      humidity: 93,
+      temp_min: 59,
+      temp_max: 63
     },
-    visibility: 10000,
-    wind: { speed: 4.1, deg: 80 },
+    visibility: 16093,
+    wind: { speed: 8.05, deg: 190 },
+    rain: { "1h": 1.2 },
     clouds: { all: 90 },
-    dt: new Date().getTime(),
+    dt: Date.now(),
     sys: {
       type: 1,
-      id: 5091,
-      message: 0.0103,
-      country: "GB",
-      sunrise: 1485762037,
-      sunset: 1485794875
+      id: 4964,
+      message: 0.0092,
+      country: "US",
+      sunrise: 1556190074,
+      sunset: 1556238519
     },
-    id: 2643743,
-    name: "London",
+    id: 4644585,
+    name: "Nashville",
     cod: 200
   };
   // return axios({ url, adapter });
@@ -136,8 +132,8 @@ export function Weather({ view = "simple" }) {
   useEffect(() => {
     let timer;
     const refresh = () => {
+      // if (!loading && !error) {
       if (url) {
-        // !loading && !error) {
         console.log("Getting weather...", url);
         getWeather(url).then(({ data }) => {
           console.log("Weather is back...");
